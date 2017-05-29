866689a99b8a1dc1698c304d5e5ec991d12460c6
rs222kn <rs222kn@student.lnu.se>
2017-05-29 20:27:18 +0200

    project set up

---
+++ server/config/db.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const mongoose = require('mongoose')
+const mongodbUrl = process.env.MONGODB_URL
+
+/**
+ * [makes an mongoose databese connection]
+ * [dispalys errors if any]
+ */
+const initilize = () => {
+  const db = mongoose.connection
+
+  db.on('error', (error) => {
+    console.log('db error', error)
+  })
+
+  db.once('open', () => {
+    console.log('db open')
+  })
+
+  process.on('SIGINT', () => {
+    db.close(() => {
+      console.log('Mongoose connection disconnected app termination.')
+      process.exit(0)
+    })
+  })
+
+  mongoose.connect(mongodbUrl)
+}
+
+module.exports = initilize
