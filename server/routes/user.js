c7b8836c4111c9767bb5ec042eb9050f6a2b4925
rs222kn <rs222kn@student.lnu.se>
2017-05-30 17:27:46 +0200

    test route

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,6 +1,24 @@
 const router = require('express').Router()
+const userHandler = require('../dal/user-handler')
+
 
 router
-  .get('/', () => { })
+  .get('/', (req, res) => {
+
+    //fbId, fbProfileImg, firstName, lastName
+
+    userHandler.createNewPlayerWithCard(
+      'my__fbId',
+      'my__fbProfileImage',
+      'richard',
+      'soderman'
+    ).then((a) => {
+      res.json(a.toObject())
+    }).catch(error => console.log('error', error))
+
+
+
+
+  })
 
 module.exports = router
