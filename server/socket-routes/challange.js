93cd910540ecb261e4f82d1f6ec93804df9506e3
rs222kn <rs222kn@student.lnu.se>
2017-05-31 22:47:11 +0200

    add challenge data and check challenge

--- server/socket-routes/challange.js
+++ server/socket-routes/challange.js
@@ -1,7 +1,8 @@
 const challangeHandler = require('../dal/challange-handler')
 const userHandler = require('../dal/user-handler')
 const cardHandler = require('../dal/card-handler')
-
+const gameRoundHandler = require('../dal/game-round-handler')
+const challangeCalculator = require('../lib/challange-calculator')
 
 const newChallange = async (io, socket, playerOneSocketId, playerOneFbId, playerTwoSocketId, playerTwoFbId) => {
   try {
@@ -15,7 +16,7 @@ const newChallange = async (io, socket, playerOneSocketId, playerOneFbId, player
       userHandler.getUserCardByFbId(playerTwoFbId)
     ])
 
-    // TODO: validate non null
+    // TODO: validate none null
 
     const challange = await challangeHandler.newChallange(...playersAndCards).save()
 
@@ -26,7 +27,6 @@ const newChallange = async (io, socket, playerOneSocketId, playerOneFbId, player
   }
 }
 
-
 const joinChallangeRoom = async (socket, challangeRoomId) => {
   try {
     const challange = await challangeHandler.getChallangeById(challangeRoomId)
@@ -36,13 +36,93 @@ const joinChallangeRoom = async (socket, challangeRoomId) => {
     }
 
     socket.join(challangeRoomId)
+    return challange.toObject()
   } catch (error) {
     throw error
   }
 }
 
+const isPlayerOne = (challange, fbId) =>
+  challange.playerOne._id.toString() === fbId
+
+const shouldRunChallange = (challange) =>
+  pureChallange.playerOneRounds.length === pureChallange.playerTwoRounds.length
+
+
+const getLatestRounds = challange => {
+  const latest = challange.playerOneRounds.length - 1
+  return [
+    challange.playerOneRounds[latest],
+    challange.playerTwoRounds[latest]
+  ]
+}
+
+
+const runChallange = async (socket, challangeId, players) => {
+
+  const challange = await challangeHandler.getChallangeById(challangeId)
+
+  const [playerOneRound, playerTwoRound] = getLatestRounds(challange)
+  const roundResult = challangeCalculator.calculateChallange(
+    challange,
+    playerOneRound,
+    playerTwoRound,
+  )
+
+
+  /*if (isPlayerOne(challange, players[0].fbId)) {
+    const [
+      {
+        clientId: playerOneClientId,
+        socketId: playerOneSocketId,
+      },
+      {
+        clientId: playerTwoClientId,
+        socketId: playerTwoSocketId,
+      }
+    ] = players
+
+  } else {
+    const [
+      {
+        clientId: playerTwoClientId,
+        socketId: playerTwoSocketId,
+      },
+      {
+        clientId: playerOneClientId,
+        socketId: playerOneSocketId,
+      }
+    ] = players
+  }*/
+
+}
+
+const addChallangeRoundData = async (challangeId, fbId, abilities) => {
+
+  const [challange, pureChallange, gameRound] = await Promise.all([
+    challangeHandler.getChallangeById(challangeId),
+    challangeHandler.getChallangeByIdNoPopulate(),
+    gameRoundHandler.newRoundData(abilities)]
+  )
+
+  // TODO: check if valid game round 
+  // if user has that many abilitie cards left
+
+  if (isPlayerOne(challange, fbId)) {
+    pureChallange.playerOneRounds.push(gameRound)
+  } else {
+    pureChallange.playerTwoRounds.push(gameRound)
+  }
+
+  await pureChallange.save()
+  return pureChallange
+}
+
 module.exports = {
   newChallange,
   joinChallangeRoom,
+  addChallangeRoundData,
+  shouldRunChallange,
+  runChallange,
 }
 
