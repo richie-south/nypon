d573d3f57a881eb40db2af4bc0eb934535f1687b
rs222kn <rs222kn@student.lnu.se>
2017-05-31 22:46:26 +0200

    implemented starting new challenge

--- server/socket-routes/connection.js
+++ server/socket-routes/connection.js
@@ -1,8 +1,11 @@
 
 const shortid = require('shortid')
 const challange = require('./challange')
+const R = require('ramda')
 const clients = {}
 
+const getClientsByChallangeId = challangeId =>
+  [R.filter(client => client.challangeId === challangeId, clients)]
 
 const connection = io => {
 
@@ -10,13 +13,18 @@ const connection = io => {
 
     const clientId = shortid.generate()
     clients[clientId] = {
+      clientId,
       socketId: socket.id,
       fbId: null,
+      challangeId: null,
     }
 
     console.log('connected', socket.id, clientId)
 
-    socket.on('client-props', function ({ fbId }) {
+    /**
+     * Recives clientId
+     */
+    socket.on('client-props', ({ fbId }) => {
       clients[clientId].fbId = fbId
 
       socket.emit('connection-props', {
@@ -24,7 +32,11 @@ const connection = io => {
       })
     })
 
-    socket.on('start-new-challange', async function ({ opponentClientId }) {
+    /**
+     * Starts new challange, 
+     * Sends new challange to both clients
+     */
+    socket.on('start-new-challange', async ({ opponentClientId }) => {
       const { socketId: playerOneSocketId, fbId: playerOneFbId } = clients[clientId]
       const { socketId: playerTwoSocketId, fbId: playerTwoFbId } = clients[opponentClientId]
       try {
@@ -37,9 +49,8 @@ const connection = io => {
           playerTwoFbId
         )
 
-        socket.broadcast.to(playerOneSocketId).emit('start-new-challange', dataToSend)
-        socket.broadcast.to(playerTwoSocketId).emit('start-new-challange', dataToSend)
-
+        socket.broadcast.to(playerOneSocketId).emit('join-new-challange', dataToSend)
+        socket.broadcast.to(playerTwoSocketId).emit('join-new-challange', dataToSend)
 
       } catch (error) {
         console.log('ERROR: start-new-challange', error)
@@ -47,11 +58,49 @@ const connection = io => {
 
     })
 
-    socket.on('join-challange-room', async function ({ challangeRoomId }) {
+    /**
+     * Clients connects to challange room with this
+     */
+    socket.on('join-challange-room', async ({ challangeRoomId }) => {
       try {
-        await challange.joinChallangeRoom(socket, challangeRoomId)
+        const challangeProps = await challange.joinChallangeRoom(socket, challangeRoomId)
+        clients[clientId].challangeId = challangeProps._id
       } catch (error) {
         console.log('ERROR: join-challange-room', error)
+      }
+    })
+
+    /**
+     * Recives position of abilitie card 
+     * send to other client
+     */
+    socket.on('ingame-round-abilitie-poistion', ({ isAdd, position }) => {
+      const { challangeId: roomId } = clients[clientId]
+
+      socket.broadcast.to(roomId).emit('ingame-round-abilitie-poistion', { isAdd, position })
+    })
+
+    /**
+     * Recives game round data 
+     */
+    socket.on('challange-round-data', async ({ abilities }) => {
+
+      const { challangeId, socketId, fbId } = clients[clientId]
+      try {
+        const pureChallange = await challange.addChallangeRoundData(challangeId, fbId, abilities)
+
+        if (challange.shouldRunChallange(pureChallange)) {
+          const challangeResult = await challange.runChallange(
+            socket,
+            challangeId,
+            getClientsByChallangeId(challangeId)
+          )
+        } else {
+          // TODO: send timer event to opponent player
+        }
+
+      } catch (error) {
+        console.log('ERROR: challange-round-data', error)
       }
     })
 
