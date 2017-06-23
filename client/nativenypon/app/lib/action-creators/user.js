44b3df514392d9982ff6ef5698643e27e74125d2
rs222kn <rs222kn@student.lnu.se>
2017-06-23 23:25:07 +0200

    add client id to user store

--- client/nativenypon/app/lib/action-creators/user.js
+++ client/nativenypon/app/lib/action-creators/user.js
@@ -3,3 +3,8 @@ export const signIn = user => ({
   type: 'SIGN_IN',
   payload: user,
 }) 
+
+export const addClientId = clientId => ({
+  type: 'ADD_CLIENT_ID',
+  payload: clientId
+})
--- client/nativenypon/app/lib/reducers/user.js
+++ client/nativenypon/app/lib/reducers/user.js
@@ -2,6 +2,8 @@ export const user = (state = {}, action) => {
   switch (action.type) {
     case 'SIGN_IN':
       return action.payload
+    case 'ADD_CLIENT_ID':
+      return Object.assign({}, state, {clientId: action.payload})
     default:
       return state
   }
