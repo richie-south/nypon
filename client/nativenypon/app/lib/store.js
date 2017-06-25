7b4a4fb12335a5306f3775bd929b3e1f12acf3a4
rs222kn <rs222kn@student.lnu.se>
2017-06-25 22:27:45 +0200

    fixed async errors with persistStore

--- client/nativenypon/app/lib/store.js
+++ client/nativenypon/app/lib/store.js
@@ -1,18 +1,32 @@
-import {compose, createStore} from 'redux'
-import {AsyncStorage} from 'react-native'
-import {persistStore, autoRehydrate} from 'redux-persist'
+import { compose, createStore } from 'redux'
+import { AsyncStorage } from 'react-native'
+import { persistStore, autoRehydrate } from 'redux-persist'
 import rootReducer from './reducers/index'
+import { presistDone } from './action-creators/initial-loading'
 
 const store = createStore(
-    rootReducer,
-    compose(
-      autoRehydrate()
-    )
+  rootReducer,
+  compose(
+    autoRehydrate()
   )
+)
 
-  persistStore(store, {storage: AsyncStorage}, (error, state) => {
-    console.log('persistStore done:', state)
-  })
+// using this way becaus async persistStore meesses things up
+let unsubscribe = store.subscribe(() => {
+  const { initialLoading } = store.getState()
 
+  if (initialLoading.isConnectionDone) {
+    unsubscribe()
+
+    const persistor = persistStore(store, {
+      storage: AsyncStorage,
+      whitelist: ['user', 'nav', 'auth']
+    }, (error, state) => {
+      console.log('persistStore done', store.getState())
+      store.dispatch(presistDone())
+    })
+
+  }
+})
 
 export default store
