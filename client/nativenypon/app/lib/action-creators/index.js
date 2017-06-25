5e845252fa5c8553eb97b98c86d1ee1972a48129
rs222kn <rs222kn@student.lnu.se>
2017-06-25 22:26:52 +0200

    added more action creators

--- client/nativenypon/app/lib/action-creators/index.js
+++ client/nativenypon/app/lib/action-creators/index.js
@@ -1,3 +1,11 @@
 import * as user from './user'
+import * as challange from './challange'
+import * as initialLoading from './initial-loading' 
+import * as abilityRoundData from './ability-round-data' 
 
-export const actionCreators = Object.assign({}, user)
+export const actionCreators = Object.assign({}, 
+  user, 
+  challange, 
+  initialLoading, 
+  abilityRoundData
+)
