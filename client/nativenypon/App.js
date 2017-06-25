31d7f3b87bbb69aee2e0077d47e99c0f9755ba85
rs222kn <rs222kn@student.lnu.se>
2017-06-25 22:24:24 +0200

    remove yellow box

--- client/nativenypon/App.js
+++ client/nativenypon/App.js
@@ -6,6 +6,14 @@ import store from './app/lib/store'
 import AppWithNavigationState from './app/navigators/AppNavigator'
 
 class App extends React.Component {
+  constructor(){
+    super()
+    console.ignoredYellowBox = [
+         'Setting a timer'
+     ];
+    console.disableYellowBox = true
+  }
+
   render() {
     return (
       <Provider store={store}>
