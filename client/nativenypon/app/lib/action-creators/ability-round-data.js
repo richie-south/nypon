e08485e87f86dd79625093492e1b952d4e88fac8
rs222kn <rs222kn@student.lnu.se>
2017-07-01 21:18:59 +0200

    clear all and update

--- client/nativenypon/app/lib/action-creators/ability-round-data.js
+++ client/nativenypon/app/lib/action-creators/ability-round-data.js
@@ -5,7 +5,20 @@ export const addAbilityCard = (type, position) => ({
 }) 
 
 export const removeAbilityCard = position => ({
-  type: 'ADD_ABILITY_CARD',
-  payload: {position},
+  type: 'REMOVE_ABILITY_CARD',
+  payload: position,
 }) 
 
+export const addAbilityCardPlayerTwo = (type, position) => ({
+  type: 'ADD_ABILITY_CARD_PLAYER_TWO',
+  payload: {type, position},
+}) 
+
+export const removeAbilityCardPlayerTwo = position => ({
+  type: 'REMOVE_ABILITY_CARD_PLAYER_TWO',
+  payload: position,
+}) 
+
+export const clearAbilityCards = () => ({
+  type: 'CLEAR_ABILITY_CARDS'
+})
